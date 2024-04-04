8d98ba3a2dc8c7851f20512aa4ab1b96a3f610f6
RannarZ <rannarzirk@gmail.com>
2024-04-04 23:48:06 +0300

    Creating Qdrant vector db

--- main.py
+++ main.py
@@ -2,15 +2,27 @@ import logging
 import sys
 import os
 
+
 from qdrant_client import QdrantClient
-from qdrant_client.http.models import Distance, VectorParams
 
-client = QdrantClient("localhost", port=6333)
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import Settings
+import vectors
+from qdrant_client import QdrantClient
+if __name__ == '__main__':
+
+    client = QdrantClient("localhost", port=6333)
 
 
-client.create_collection(
-    collection_name="test_collection",
-    vectors_config=VectorParams(size=4, distance=Distance.DOT),
-)
+    Settings.embed_model = OpenAIEmbedding(model = 'text-embedding-ada-002', api_key = openai_api_key)
+    Settings.llm = OpenAI(model= 'gpt-3.5-turbo', api_key=openai_api_key)
 
-print(client)
+    #create_storage(client)
+    #print("Complete!")
+    #client.delete_collection(collection_name="docs_vectors")
+    index = VectorStoreIndex.from_documents(SimpleDirectoryReader(input_dir="qdrant_storage/collections/docs_vectors").load_data())
+    query = "Mis teleskoope kasutatakse Tartu Ülikoolis?"
+    vectors.prompt(index, query)
