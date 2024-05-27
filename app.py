450d21e68cb00d228ce2e5af8f23e1d4b4a63182
Kaarel-Richard Kaarelson <kaarel-richard.kaarelson@ut.ee>
2024-05-27 11:47:16 +0300

    Change prompts

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
 import os 
-# from dotenv import load_dotenv
-
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
-from llama_index.core import StorageContext
-from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader, StorageContext
+import streamlit as st
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, StorageContext, Settings, ChatPromptTemplate
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
-from llama_index.core import ChatPromptTemplate
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-from langchain_core.callbacks import BaseCallbackManager
 from llama_index.vector_stores.pinecone import PineconeVectorStore
-import streamlit as st
+from langchain_core.callbacks import BaseCallbackManager
 from pinecone import Pinecone
 
-# load_dotenv()
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+pinecone_api_key = os.environ["PINECONE_API_KEY"]
 
-# openai_api_key=os.getenv("OPENAI_API_KEY")
-openai_api_key=st.secrets["OPENAI_API_KEY"]
-
-Settings.embed_model = OpenAIEmbedding(model = 'text-embedding-3-small', api_key=openai_api_key)
+Settings.embed_model = OpenAIEmbedding(model='text-embedding-3-small', api_key=openai_api_key)
 Settings.llm = OpenAI(model='gpt-3.5-turbo', api_key=openai_api_key)
 
 qa_prompt_str = (
@@ -29,14 +22,13 @@ qa_prompt_str = (
     "Given the context information and not prior knowledge, please answer the question: {query_str}\n"
 )
 
-
 start_messages = [
     (
         "system", 
         "You are an expert Q&A system of University of Tartu.\n"
         "Some rules to follow:\n"
         "1. Avoid statements like 'Based on the context, ...' or 'The context information ...' or anything along those lines.\n"
-     ),
+    ),
     (
         "user", 
         "Context information is below.\n"
@@ -44,8 +36,7 @@ start_messages = [
         "{context_str}\n"
         "---------------------\n"
         "Given the context information and not prior knowledge, please answer the question: {query_str}\n"
-     
-     ),
+    ),
 ]
 
 text_qa_template = ChatPromptTemplate.from_messages(start_messages)
@@ -55,27 +46,12 @@ class ChatbotUT:
         self.initialize()
 
     def initialize(self):
-        try:
-            api_key = os.environ["PINECONE_API_KEY"]
-            pc = Pinecone(api_key=api_key)
-            pinecone_index = pc.Index("quickstart")
-            index_exists = True
-        except ValueError:
-            index_exists = False
-
-
-        if not index_exists: 
-            print("Pinecone index does not exist")
-            raise ValueError("Pinecone index does not exist")
-        else: 
-            self.vector_store = PineconeVectorStore(pinecone_index=pinecone_index) 
-            self.storage_context = StorageContext.from_defaults(vector_store=self.vector_store)
-
-            self.index = VectorStoreIndex.from_vector_store(
-                self.vector_store, storage_context=self.storage_context
-            )
-
-            self.chat_engine = self.index.as_chat_engine(chat_mode="condense_question", text_qa_template = text_qa_template, verbose=True)
+        pc = Pinecone(api_key=pinecone_api_key)
+        pinecone_index = pc.Index("quickstart")
+        self.vector_store = PineconeVectorStore(pinecone_index=pinecone_index) 
+        self.storage_context = StorageContext.from_defaults(vector_store=self.vector_store)
+        self.index = VectorStoreIndex.from_vector_store(self.vector_store, storage_context=self.storage_context)
+        self.chat_engine = self.index.as_chat_engine(chat_mode="condense_question", text_qa_template=text_qa_template, verbose=True)
         
     def get_chat(self):
         return self.chat_engine
@@ -91,28 +67,30 @@ class StreamHandler(BaseCallbackManager):
         self.text += token
         self.container.write_stream()
 
-# Define the possible options for the user to select from
-options = [
-    #{"title": "Kui palju on Tartu Ülikoolis tudengeid?"},
-    {"title": "How many students are in University of Tartu?"},
-    {"title": "What language test I have to complete to go on a study abroad program with Erasmus?"},
-    {"title": "What telescopes does university of Tartu have?"}
-]
+def display_chat_messages() -> None:
+    """Print message history
+    @returns None
+    """
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"], avatar=message["avatar"]):
+            st.markdown(message["content"])
 
-st.set_page_config(
-    page_title="main.py",
-)
+
+st.set_page_config(page_title="University of Tartu Chatbot")
 
 st.header("UT RAG 🎓🐬")
 
 if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "assistant", "avatar": "🐬", "content": "I am a helpful student assistant who knows everything about University of Tartu, ask me anything!"}
-    ]
+    st.session_state.messages = [{"role": "assistant", "avatar": "🐬", "content": "I am a helpful student assistant who knows everything about University of Tartu, ask me anything!"}]
+
+options = [
+    {"title": "Kui palju õppekavasid on Tartu ülikoolis?"},
+    {"title": "¿Donde está la biblioteca?"},
+    {"title": "What telescopes does university of Tartu have?"}
+]
 
 user_prompt = ""
 
-# Display the suggestive prompt cards
 col1, col2, col3 = st.columns(3)
 with col1:
     if st.button(options[0]["title"]):
@@ -124,26 +102,22 @@ with col3:
     if st.button(options[2]["title"]):
         user_prompt = options[2]["title"]
 
+display_chat_messages()
+
 if "current_response" not in st.session_state:
     st.session_state.current_response = ""
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"], avatar=message['avatar']):
-        st.markdown(message["content"])
 
 if user_prompt := st.chat_input("Ask question here", key="user_input") or user_prompt:
 
     with st.chat_message("user", avatar="🎓"):
         st.markdown(user_prompt)
 
-    st.session_state.messages.append(
-        {"role": "user", "avatar":"🎓", "content": user_prompt}
-    )
+    st.session_state.messages.append({"role": "user", "avatar":"🎓", "content": user_prompt})
 
     streaming_response = chatbot_ut.stream_chat(user_prompt)
     with st.chat_message("assistant", avatar="🐬"): 
         st.write_stream(streaming_response.response_gen)
 
-    st.session_state.messages.append(
-        {"role": "assistant", "avatar":"🐬", "content": streaming_response}
-    )
+    st.session_state.messages.append({"role": "assistant", "avatar":"🐬", "content": streaming_response})
+        
+    st.rerun()
--- scripts/run_app.py
+++ scripts/run_app.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 def run_scrapy_crawl():
-    command = "python -m streamlit run ./src/app.py"
+    command = "python -m streamlit run ./app.py"
     subprocess.run(command, shell=True)
 
 if __name__ == "__main__":
