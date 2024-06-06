c0d74c9e05f0a0e0553fbf5be964c7c5bd9e8786
Kaarel-Richard Kaarelson <kaarel-richard.kaarelson@ut.ee>
2024-06-06 21:36:01 +0300

    Reimplement streaming

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import os
 import sys
+import uuid
 import streamlit as st
-from langchain_core.callbacks import BaseCallbackManager
-from google.cloud import translate_v2 as translate
 from langdetect import detect
 from langchain.prompts import ChatPromptTemplate
 from langchain_pinecone import PineconeVectorStore
@@ -16,24 +15,84 @@ from langchain_core.prompts import MessagesPlaceholder
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
-from langchain_openai import ChatOpenAI, OpenAI
-from langchain.chains import RetrievalQA  
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import AIMessage
-import uuid
 from loguru import logger
 
-# Configure loguru
-logger.remove()  # Remove the default logger
+logger.remove() 
 logger.add(
     sink=sys.stdout,
-    format="<level>{message}</level>",  # Customize the format to exclude date and time
-    level="DEBUG"  # Set default logging level to DEBUG
+    format="<level>{message}</level>", 
+    level="DEBUG"  
 )
 
 OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
 PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
 
-lang_dict = {}
+lang_dict = {
+    "af": "Afrikaans",
+    "sq": "Albanian",
+    "am": "Amharic",
+    "ar": "Arabic",
+    "hy": "Armenian",
+    "az": "Azerbaijani",
+    "eu": "Basque",
+    "be": "Belarusian",
+    "bn": "Bengali",
+    "bs": "Bosnian",
+    "bg": "Bulgarian",
+    "my": "Burmese",
+    "ca": "Catalan",
+    "ceb": "Cebuano",
+    "ny": "Chichewa",
+    "zh-cn": "Chinese (Simplified)",
+    "zh-tw": "Chinese (Traditional)",
+    "co": "Corsican",
+    "hr": "Croatian",
+    "cs": "Czech",
+    "da": "Danish",
+    "nl": "Dutch",
+    "en": "English",
+    "es": "Spanish",
+    "eo": "Esperanto",
+    "et": "Estonian",
+    "tl": "Filipino",
+    "fi": "Finnish",
+    "fr": "French",
+    "fy": "Frisian",
+    "gl": "Galician",
+    "ka": "Georgian",
+    "de": "German",
+    "el": "Greek",
+    "gu": "Gujarati",
+    "ht": "Haitian Creole",
+    "ha": "Hausa",
+    "haw": "Hawaiian",
+    "he": "Hebrew",
+    "hi": "Hindi",
+    "hmn": "Hmong",
+    "hu": "Hungarian",
+    "is": "Icelandic",
+    "ig": "Igbo",
+    "id": "Indonesian",
+    "ga": "Irish",
+    "it": "Italian",
+    "ja": "Japanese",
+    "jv": "Javanese",
+    "kn": "Kannada",
+    "kk": "Kazakh",
+    "km": "Khmer",
+}
+
+def detect_language(text: str) -> dict:
+    """Returns the text's language. E.g. 'English', 'Estonian' """
+
+    lang_abbr = detect(text)
+    lang = lang_dict[lang_abbr] if lang_dict.get(lang_abbr) else "English"
+
+    logger.debug(f"PROMPT LANGUAGE: {lang}, ABBREVIATION: {lang_abbr}")
+
+    return lang
 
 class Chatbot:
     model_name = "gpt-3.5-turbo"
@@ -82,7 +141,7 @@ class Chatbot:
             "{context}\n"
             "---------------------\n"
             "Given the context information and not prior knowledge,"
-            "Please answer the question, please answer user question"
+            "Please answer the question in the following language: {language}"
         )
        
        qa_prompt = ChatPromptTemplate.from_messages(
@@ -93,7 +152,7 @@ class Chatbot:
             ]
         )
        
-       question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+       question_answer_chain = create_stuff_documents_chain(llm, qa_prompt) #| StrOutputParser()
        rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
        
        self.conversational_rag_chain = RunnableWithMessageHistory(
@@ -119,20 +178,15 @@ class Chatbot:
             logger.debug(f"{prefix}: {message.content}\n")
         
     def get_answer(self, question):
-        return self.conversational_rag_chain.invoke({"input": question},
-                                               config={"configurable": {"session_id": self.session_id
-                                                                        }}, 
-                                               )["answer"]
+        language = detect_language(question)
 
-class StreamHandler(BaseCallbackManager):
-    def __init__(self, container, initial_text=""):
-        self.container = container
-        self.text = initial_text
-
-    def on_llm_new_token(self, token: str, **kwargs) -> None:
-        self.text += token
-        self.container.write_stream()
-
+        stream = self.conversational_rag_chain.stream({"input": question, "language": language},
+                                               config={"configurable": {"session_id": self.session_id}})
+        
+        for chunk in stream:
+            if "answer" in chunk:
+                yield chunk['answer']
+            
 def display_chat_messages() -> None:
     """Print message history
     @returns None
@@ -140,18 +194,6 @@ def display_chat_messages() -> None:
     for message in st.session_state.messages:
         with st.chat_message(message["role"], avatar=message["avatar"]):
             st.markdown(message["content"])
-
-def detect_language(text: str) -> dict:
-    """Returns the text's language."""
-
-    translate_client = translate.Client()
-
-    result = translate_client.detect_language(text)
-
-    logger.debug("Confidence: {}".format(result["confidence"]))
-    logger.debug("Language: {}".format(result["language"]))
-
-    return result["language"]
 
 if "chatbot" not in st.session_state:
     st.session_state.chatbot = Chatbot()
@@ -168,31 +210,16 @@ options = [
 ]
 
 prompt_option = ""
-user_prompt = ""
+question = ""
 st.session_state.response_status = "completed"
 
-def get_answer(text: str) -> str:
-    """Returns the answer from AI"""
-
-    lang_abbr = detect(user_prompt)
-    lang = lang_dict[lang_abbr]
-    logger.debug("PROMPT LANGUAGE:", lang, "ABBREVIATION:", lang_abbr)
-
-    return chatbot.stream_chat(
-        user_prompt + 
-        f"\n Please answer this question in the following language: {lang}"
-        )
-
 st.set_page_config(page_title="University of Tartu Chatbot")
-
 st.header("UT Chatbot 🎓🐬")
-
 st.markdown("""
 <center>
 <a href="https://github.com/kaarelkaarelson/ut-rag" target="_blank">contribute to open-source project</a>
 </center>
 """, unsafe_allow_html=True)
-
 st.text(" ")
 
 col1, col2, col3 = st.columns(3)
@@ -211,7 +238,7 @@ display_chat_messages()
 if "current_response" not in st.session_state:
     st.session_state.current_response = ""
 
-if user_prompt := st.chat_input("Ask question here", key="user_input", disabled=st.session_state.response_status == "processing") or prompt_option:
+if question := st.chat_input("Ask question here", key="user_input", disabled=st.session_state.response_status == "processing") or prompt_option:
     logger.debug('STATUS', st.session_state.response_status, "Prompt option", prompt_option)
     
     if prompt_option:
@@ -219,22 +246,16 @@ if user_prompt := st.chat_input("Ask question here", key="user_input", disabled=
     
     st.session_state.response_status = "processing"
     with st.chat_message("user", avatar="🎓"):
-        st.markdown(user_prompt)
+        st.markdown(question)
 
-    st.session_state.messages.append({"role": "user", "avatar":"🎓", "content": user_prompt})
-
-    # streaming_response = get_answer(user_prompt)
-    streaming_response = chatbot.get_answer(user_prompt) #qa.run(user_prompt)  
-
+    st.session_state.messages.append({"role": "user", "avatar":"🎓", "content": question})
 
     with st.chat_message("assistant", avatar="🐬",): 
-        st.write(streaming_response)
-    # with st.chat_message("assistant", avatar="🐬",): 
-    #     st.write_stream(streaming_response.response_gen)
+        response = st.write_stream(chatbot.get_answer(question))
 
     logger.debug("History:")
     chatbot.print_message_history()
 
-    st.session_state.messages.append({"role": "assistant", "avatar":"🐬", "content": streaming_response})
+    st.session_state.messages.append({"role": "assistant", "avatar":"🐬", "content": response})
     st.session_state.response_status = "completed"
     st.rerun()
